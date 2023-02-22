985ed0fec09953bcd040dde475da3fdc64d4fc52
Thomas <91778476+Hiikss@users.noreply.github.com>
2023-02-22 10:14:24 +0100

    Update ConfigurationREST.java

--- AppliDemo/src/main/java/fr/eni/javaee/module9/ConfigurationREST.java
+++ AppliDemo/src/main/java/fr/eni/javaee/module9/ConfigurationREST.java
@@ -1,7 +1,9 @@
 package fr.eni.javaee.module9;
 
+import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+@ApplicationPath("/module/module8/rest")
 public class ConfigurationREST extends Application {
 
 }
