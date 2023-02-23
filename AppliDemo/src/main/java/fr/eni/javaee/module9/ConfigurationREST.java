18953c646f3be1b2b10b34c7916997e9a0639202
Thomas <91778476+Hiikss@users.noreply.github.com>
2023-02-23 09:53:42 +0100

    ajout demo 2 module 9

--- AppliDemo/src/main/java/fr/eni/javaee/module9/ConfigurationREST.java
+++ AppliDemo/src/main/java/fr/eni/javaee/module9/ConfigurationREST.java
@@ -3,7 +3,7 @@ package fr.eni.javaee.module9;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/module/module8/rest")
+@ApplicationPath("/modules/module9/rest")
 public class ConfigurationREST extends Application {
 
 }
